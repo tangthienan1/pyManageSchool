4064c4f88b0be0d6ded29b9f8e8fe6562def1bc4
andytang9 <90296770+andytang9@users.noreply.github.com>
2021-11-15 22:57:30 +0700

    csv

--- managementstudent/settings.py
+++ managementstudent/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT ='/media'
+
+#DataFlair
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tangthienan9@gmail.com'
+EMAIL_HOST_PASSWORD = 'Daylamatkhau@1912'
--- topic/admin.py
+++ topic/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
+from django.contrib.admin.decorators import action
 from django.contrib.admin.helpers import Fieldset
 from .models import Category, Topic, Idea
 # Register your models here.
 admin.site.site_header = "Manage Topic"
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 class TopicAdmin(admin.ModelAdmin):
     Fieldset = [
         (None, {'fields':['title','closure_date','final_closure_date']})
     ]
-    inlines = [Idea]
-admin.site.register(Topic)
-admin.site.register(Idea)
+
+class IdeaAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Idea, IdeaAdmin)
 admin.site.register(Category)
