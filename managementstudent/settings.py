3555059fa2d53457bdd3a2dede81e0d047c497a1
andytang9 <90296770+andytang9@users.noreply.github.com>
2021-11-16 00:21:27 +0700

    done

--- managementstudent/settings.py
+++ managementstudent/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tangthienan9@gmail.com'
-EMAIL_HOST_PASSWORD = 'Daylamatkhau@1912'
+EMAIL_HOST_USER = 'managementidea@gmail.com'
+EMAIL_HOST_PASSWORD = '123456789@A'
